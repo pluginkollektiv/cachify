779ddf3541b9eb71fc0dae5f143b245e3d0c85ff
timse201 <timse201@users.noreply.github.com>
2017-01-29 00:48:29 +0100

    Update cachify.memcached.nginx.php

--- inc/setup/cachify.memcached.nginx.php
+++ inc/setup/cachify.memcached.nginx.php
@@ -29,7 +29,7 @@ defined('ABSPATH') OR exit;
 		</tr>
 	</table>
 
-	<div name="cachify[memcached_nginx]" style="background:#fff;border:1px solid #ccc;padding:10px 20px"><pre>
+	<div style="background:#fff;border:1px solid #ccc;padding:10px 20px"><pre>
 ## GZIP
 gzip_static on;
 
@@ -64,3 +64,14 @@ location @nocache {
     try_files $uri $uri/ /index.php?$args;
 }
 </pre></div>
+
+	<table class="form-table">
+		<tr>
+			<td>
+				<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=LG5VC9KXMAYXJ" target="_blank" rel="noopener noreferrer"><?php esc_html_e( 'Donate', 'cachify' ); ?></a>
+				&bull; <a href="<?php esc_html_e( 'https://wordpress.org/plugins/cachify/faq/', 'cachify' ); ?>" target="_blank" rel="noopener noreferrer"><?php esc_html_e( 'FAQ', 'cachify' ); ?></a>
+				&bull; <a href="https://github.com/pluginkollektiv/cachify/wiki" target="_blank" rel="noopener noreferrer"><?php esc_html_e( 'Manual', 'cachify' ); ?></a>
+				&bull; <a href="https://wordpress.org/support/plugin/cachify" target="_blank" rel="noopener noreferrer"><?php esc_html_e( 'Support', 'cachify' ); ?></a>
+			</td>
+		</tr>
+	</table>
