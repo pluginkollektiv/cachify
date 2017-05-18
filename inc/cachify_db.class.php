pluginkollektiv/cachify

commit 702a207ba2f1a3c6206de6d584a92a2a51c0d8f2

Translate German residues in comments and private method names into English.

inc/cachify_db.class.php
@@ -70,11 +70,11 @@ final class Cachify_DB {
 	 * @since   2.0
 	 * @change  2.0
 	 *
-	 * @param   string $hash  Hash of the entry.
-	 * @return  mixed          Content of the entry
+	 * @param   string $hash    Hash of the entry.
+	 * @return  mixed           Content of the entry
 	 */
 	public static function get_item( $hash ) {
-		/* Leer? */
+		/* Empty? */
 		if ( empty( $hash ) ) {
 			wp_die( 'DB get item: Empty input.' );
 		}
@@ -134,7 +134,7 @@ final class Cachify_DB {
 
 		/* Signature */
 		if ( isset( $cache['meta'] ) ) {
-			echo self::_cache_signatur( $cache['meta'] );
+			echo self::_cache_signature( $cache['meta'] );
 		}
 
 		/* Quit */
@@ -168,7 +168,7 @@ final class Cachify_DB {
 	 * @param   array $meta  Content of metadata.
 	 * @return  string       Signature string
 	 */
-	private static function _cache_signatur( $meta ) {
+	private static function _cache_signature( $meta ) {
 		/* No array? */
 		if ( ! is_array( $meta ) ) {
 			return;
@@ -178,19 +178,19 @@ final class Cachify_DB {
 			"\n\n<!--\n%s\n%s\n%s\n%s\n-->",
 			'Cachify | http://cachify.de',
 			sprintf(
-				'Ohne Plugin: %d DB-Anfragen, %s Sekunden, %s',
+				'Without Cachify: %d DB queries, %s seconds, %s',
 				$meta['queries'],
 				$meta['timer'],
 				$meta['memory']
 			),
 			sprintf(
-				'Mit Plugin: %d DB-Anfragen, %s Sekunden, %s',
+				'With Cachify: %d DB queries, %s seconds, %s',
 				self::_page_queries(),
 				self::_page_timer(),
 				self::_page_memory()
 			),
 			sprintf(
-				'Generiert: %s zuvor',
+				'Cachify saved %s of execution time',
 				human_time_diff( $meta['time'], current_time( 'timestamp' ) )
 			)
 		);
@@ -202,14 +202,14 @@ final class Cachify_DB {
 	 * @since   0.1
 	 * @change  2.0
 	 *
-	 * @return  integer  Numbe rof queries
+	 * @return  integer  Number of queries
 	 */
 	private static function _page_queries() {
 		return $GLOBALS['wpdb']->num_queries;
 	}
 
 	/**
-	 * REturn execution time
+	 * Return execution time
 	 *
 	 * @since   0.1
 	 * @change  2.0

inc/cachify.class.php
@@ -53,7 +53,7 @@ final class Cachify {
 	const MINIFY_HTML_JS = 2;
 
 	/**
-	 * Pseudo-Constructor
+	 * Pseudo constructor
 	 *
 	 * @since   2.0.5
 	 * @change  2.0.5
@@ -299,7 +299,7 @@ final class Cachify {
 	public static function on_activation() {
 		/* Multisite & Network */
 		if ( is_multisite() && ! empty( $_GET['networkwide'] ) ) {
-			/* Blog-IDs */
+			/* Blog IDs */
 			$ids = self::_get_blog_ids();
 
 			/* Loop over blogs */
@@ -317,7 +317,7 @@ final class Cachify {
 	}
 
 	/**
-	 * Plugin-Installation on new MU-Blogs
+	 * Plugin installation on new MU blog.
 	 *
 	 * @since   1.0
 	 * @change  1.0
@@ -325,7 +325,7 @@ final class Cachify {
 	 * @param integer $id  Blog ID.
 	 */
 	public static function install_later( $id ) {
-		/* No network-plugin */
+		/* No network plugin */
 		if ( ! is_plugin_active_for_network( CACHIFY_BASE ) ) {
 			return;
 		}
@@ -357,7 +357,7 @@ final class Cachify {
 	}
 
 	/**
-	 * Uninstalling of the plugin per MU-Blog
+	 * Uninstalling of the plugin per MU blog.
 	 *
 	 * @since   1.0
 	 * @change  2.1.0
@@ -371,7 +371,7 @@ final class Cachify {
 			/* Alter Blog */
 			$old = $wpdb->blogid;
 
-			/* Blog-IDs */
+			/* Blog IDs */
 			$ids = self::_get_blog_ids();
 
 			/* Loop */
@@ -388,7 +388,7 @@ final class Cachify {
 	}
 
 	/**
-	 * Uninstalling of the plugin für MU and Network
+	 * Uninstalling of the plugin for MU and network.
 	 *
 	 * @since   1.0
 	 * @change  1.0
@@ -431,7 +431,7 @@ final class Cachify {
 	 * @since   1.0
 	 * @change  1.0
 	 *
-	 * @return  array  Blog-IDs
+	 * @return  array  Blog IDs
 	 */
 	private static function _get_blog_ids() {
 		/* Global */
@@ -639,7 +639,7 @@ final class Cachify {
 	 * @since   2.0.6
 	 * @change  2.0.6
 	 *
-	 * @return  integer    Cache size in Bytes.
+	 * @return  integer    Cache size in bytes.
 	 */
 	public static function get_cache_size() {
 		if ( ! $size = get_transient( 'cachify_cache_size' ) ) {
@@ -731,7 +731,7 @@ final class Cachify {
 			/* Old blog */
 			$old = $GLOBALS['wpdb']->blogid;
 
-			/* Blog-IDs */
+			/* Blog IDs */
 			$ids = self::_get_blog_ids();
 
 			/* Loop over blogs */
@@ -790,7 +790,7 @@ final class Cachify {
 	 * @hook    mixed  cachify_user_can_flush_cache
 	 */
 	public static function flush_notice() {
-		/* No Admin */
+		/* No admin */
 		if ( ! is_admin_bar_showing() || ! apply_filters( 'cachify_user_can_flush_cache', current_user_can( 'manage_options' ) ) ) {
 			return false;
 		}

inc/cachify_apc.class.php
@@ -43,7 +43,7 @@ final class Cachify_APC {
 	 * @param   integer $lifetime  Lifetime of the entry.
 	 */
 	public static function store_item( $hash, $data, $lifetime ) {
-		/* Leer? */
+		/* Empty? */
 		if ( empty( $hash ) || empty( $data ) ) {
 			wp_die( 'APC add item: Empty input.' );
 		}
@@ -51,7 +51,7 @@ final class Cachify_APC {
 		/* Store */
 		apc_store(
 			$hash,
-			gzencode( $data . self::_cache_signatur(), 9 ),
+			gzencode( $data . self::_cache_signature(), 9 ),
 			$lifetime
 		);
 	}
@@ -145,7 +145,7 @@ final class Cachify_APC {
 	 *
 	 * @return  string  Signature string
 	 */
-	private static function _cache_signatur() {
+	private static function _cache_signature() {
 		return sprintf(
 			"\n\n<!-- %s\n%s @ %s -->",
 			'Cachify | http://cachify.de',

inc/cachify_hdd.class.php
@@ -43,14 +43,14 @@ final class Cachify_HDD {
 	 * @param   integer $lifetime  Lifetime of the entry [optional].
 	 */
 	public static function store_item( $hash, $data, $lifetime ) {
-		/* Leer? */
+		/* Empty? */
 		if ( empty( $data ) ) {
 			wp_die( 'HDD add item: Empty input.' );
 		}
 
-		/* Speichern */
+		/* Store data */
 		self::_create_files(
-			$data . self::_cache_signatur()
+			$data . self::_cache_signature()
 		);
 	}
 
@@ -60,7 +60,7 @@ final class Cachify_HDD {
 	 * @since   2.0
 	 * @change  2.0
 	 *
-	 * @return  boolean  $diff  TRUE if cache is present
+	 * @return  boolean  True if cache is present.
 	 */
 	public static function get_item() {
 		return is_readable(
@@ -132,7 +132,7 @@ final class Cachify_HDD {
 	 *
 	 * @return  string  Signature string
 	 */
-	private static function _cache_signatur() {
+	private static function _cache_signature() {
 		return sprintf(
 			"\n\n<!-- %s\n%s @ %s -->",
 			'Cachify | http://cachify.de',
@@ -169,8 +169,8 @@ final class Cachify_HDD {
 	 * @since   2.0
 	 * @change  2.0
 	 *
-	 * @param   string $file  Pfad der Cache-Datei.
-	 * @param   string $data  Cache-Inhalt.
+	 * @param   string $file  Path to cache file.
+	 * @param   string $data  Cache content.
 	 */
 	private static function _create_file( $file, $data ) {
 		/* Writable? */
@@ -198,7 +198,7 @@ final class Cachify_HDD {
 	 * @change  2.0.5
 	 *
 	 * @param   string  $dir        Directory path.
-	 * @param   boolean $recursive  clear subdirectories.
+	 * @param   boolean $recursive  Clear subdirectories?
 	 */
 	private static function _clear_dir( $dir, $recursive = false ) {
 		/* Remote training slash */
@@ -238,7 +238,7 @@ final class Cachify_HDD {
 			@rmdir( $dir );
 		}
 
-		/* CleanUp */
+		/* Clean up */
 		clearstatcache();
 	}
 
@@ -293,7 +293,7 @@ final class Cachify_HDD {
 	 * @since   2.0
 	 * @change  2.0
 	 *
-	 * @param   string $path  Request-URI or Permalink [optional].
+	 * @param   string $path  Request URI or permalink [optional].
 	 * @return  string        Path to cache file
 	 */
 	private static function _file_path( $path = null ) {
@@ -315,7 +315,7 @@ final class Cachify_HDD {
 		);
 
 		if ( validate_file( $path ) > 0 ) {
-			wp_die( 'Invalide file path.' );
+			wp_die( 'Invalid file path.' );
 		}
 
 		return trailingslashit( $path );

inc/cachify_memcached.class.php
@@ -65,7 +65,7 @@ final class Cachify_MEMCACHED {
 		/* Add item */
 		self::$_memcached->set(
 			self::_file_path(),
-			$data . self::_cache_signatur(),
+			$data . self::_cache_signature(),
 			$lifetime
 		);
 	}
@@ -179,7 +179,7 @@ final class Cachify_MEMCACHED {
 	 *
 	 * @return  string  Signature string
 	 */
-	private static function _cache_signatur() {
+	private static function _cache_signature() {
 		return sprintf(
 			"\n\n<!-- %s\n%s @ %s -->",
 			'Cachify | http://cachify.de',
@@ -197,7 +197,7 @@ final class Cachify_MEMCACHED {
 	 * @since   2.0.7
 	 * @change  2.0.7
 	 *
-	 * @param   string $path  Request-URI or Permalink [optional].
+	 * @param   string $path  Request URI or permalink [optional].
 	 * @return  string        Path to cache file
 	 */
 	private static function _file_path( $path = null ) {

commit 6e95ff81734dfc8f324382105423999dad101d78

Fix wrong translation introduced in 702a207ba2f1a3c6206de6d584a92a2a51c0d8f2.

inc/cachify_db.class.php
@@ -187,7 +187,7 @@ final class Cachify_DB {
 				self::_page_memory()
 			),
 			sprintf(
-				'Cachify saved %s of execution time',
+				'Generated: %s ago',
 				human_time_diff( $meta['time'], current_time( 'timestamp' ) )
 			)
 		);