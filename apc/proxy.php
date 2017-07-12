d868ed6f1015f63525c75771b1e469d3a348bc2a
Stefan Kalscheuer <stefan@stklcode.de>
2017-07-12 21:49:09 +0200

    Fix hash function for APC proxy
    The added prefix for HTTPS pages, introduced in #2, has not been added for the APC proxy which breaks APC functionallity for HTTPS pages (reported in #135).

--- apc/proxy.php
+++ apc/proxy.php
@@ -9,12 +9,35 @@ if ( ! empty( $_COOKIE ) ) {
 	}
 }
 
+/**
+ * Determines if SSL is used.
+ *
+ * @see is_ssl() (wp-includes/load.php).
+ *
+ * @return bool True if SSL, otherwise false.
+ */
+function cachify_is_ssl() {
+	if ( isset( $_SERVER['HTTPS'] ) ) {
+		if ( 'on' == strtolower( $_SERVER['HTTPS'] ) ) {
+			return true;
+		}
+
+		if ( '1' == $_SERVER['HTTPS'] ) {
+			return true;
+		}
+	} elseif ( isset( $_SERVER['SERVER_PORT'] ) && ( '443' == $_SERVER['SERVER_PORT'] ) ) {
+		return true;
+	}
+
+	return false;
+}
+
 if (
 	empty( $_cachify_logged_in )
 	&& ( strpos( filter_input( INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_STRING ), '/wp-admin/' ) === false )
 	&& ( strpos( filter_input( INPUT_SERVER, 'HTTP_ACCEPT_ENCODING', FILTER_SANITIZE_STRING ), 'gzip' ) !== false )
 	&& extension_loaded( 'apc' )
-	&& ( $cache = apc_fetch( md5( $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ) . '.cachify' ) )
+	&& ( $cache = apc_fetch( md5( ( cachify_is_ssl() ? 'https-' : '' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ) . '.cachify' ) )
 ) {
 	ini_set( 'zlib.output_compression', 'Off' );
 
