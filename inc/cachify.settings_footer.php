907b3936c57c6c5e207dba270086deff43b0fb9e
timse201 <timse201@users.noreply.github.com>
2020-04-19 08:55:22 +0200

    updated links (#194)

--- inc/cachify.settings_footer.php
+++ inc/cachify.settings_footer.php
@@ -4,8 +4,8 @@ defined( 'ABSPATH' ) || exit;
 ?>
 
 <p>
-	<a href="<?php echo esc_url( __( 'https://cachify.pluginkollektiv.org/documentation#faq', 'cachify' ), 'https' ); ?>" rel="noopener noreferrer"><?php esc_html_e( 'FAQ', 'cachify' ); ?></a>
+	<a href="<?php echo esc_url( __( 'https://cachify.pluginkollektiv.org/documentation/#faq', 'cachify' ), 'https' ); ?>" rel="noopener noreferrer"><?php esc_html_e( 'FAQ', 'cachify' ); ?></a>
 	&bull; <a href="https://cachify.pluginkollektiv.org/documentation/" rel="noopener noreferrer"><?php esc_html_e( 'Manual', 'cachify' ); ?></a>
-	&bull; <a href="https://wordpress.org/support/plugin/cachify" rel="noopener noreferrer"><?php esc_html_e( 'Support', 'cachify' ); ?></a>
+	&bull; <a href="https://wordpress.org/support/plugin/cachify/" rel="noopener noreferrer"><?php esc_html_e( 'Support', 'cachify' ); ?></a>
 	&bull; <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=TD4AMD2D8EMZW" target="_blank" rel="noopener noreferrer"><?php esc_html_e( 'Donate', 'cachify' ); ?></a>
 </p>
