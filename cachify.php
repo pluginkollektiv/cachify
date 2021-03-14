041d60ad0d675b4bf153d1db95c3faf250f37566
Stefan Kalscheuer <stefan@stklcode.de>
2021-03-14 20:51:41 +0100

    Release 2.3.1, rewrite changelog for 2.3.0, WP 5.7 compatibility (#195)
    * update deployment branches from master to stable/develop
    * prepare release 2.3.1
    * update version and stable tag
    * CHANGELOG++
    * README++
    * Update screenshot of settings page
    * bump WP compatibility to 5.7 + updated screenshots

--- cachify.php
+++ cachify.php
@@ -7,7 +7,7 @@
  * Plugin URI:  https://cachify.pluginkollektiv.org
  * License:     GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Version:     2.3.0
+ * Version:     2.3.1
  * Text Domain: cachify
  *
  * @package Cachify
