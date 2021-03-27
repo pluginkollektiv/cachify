5bc1bf7978f7c92a57659e6cc5d1c940981eec2c
timse201 <timse201@users.noreply.github.com>
2021-03-27 18:00:50 +0100

    echo sprintf to printf

--- inc/setup/cachify.apc.htaccess.php
+++ inc/setup/cachify.apc.htaccess.php
@@ -32,7 +32,7 @@ $ending = '/cachify/apc/proxy.php
 	<div style="background:#fff;border:1px solid #ccc;padding:10px 20px">
 		<pre style="white-space: pre-wrap">
 		<?php
-		echo sprintf(
+		printf(
 			'%s%s%s',
 			esc_html( $beginning ),
 			esc_html( WP_PLUGIN_DIR ),
