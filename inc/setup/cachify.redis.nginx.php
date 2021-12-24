a021c2267446acd0b49a658f4d9c7a142e6e4ed3
binaryfrost <binaryfrost@gmail.com>
2021-12-24 15:18:43 +0000

    Amended notification for server environment vars

--- inc/setup/cachify.redis.nginx.php
+++ inc/setup/cachify.redis.nginx.php
@@ -9,67 +9,13 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 
-<h2><?php esc_html_e( 'nginx Redis setup', 'cachify' ); ?></h2>
-<p><?php esc_html_e( 'Please add the following lines to your nginx.conf', 'cachify' ); ?></p>
+<h2><?php esc_html_e( 'Redis setup', 'cachify' ); ?></h2>
+<p><?php esc_html_e( 'Please ensure the following variables are added to your environment', 'cachify' ); ?></p>
 
 <textarea rows="16" class="large-text code cachify-code" name="code" readonly>
-## GZIP
-gzip_static on;
-
-## CHARSET
-charset utf-8;
-
-## INDEX LOCATION
-location / {
-  error_page 404 405 = @nocache;
-
-  if ( $query_string ) {
-	return 405;
-  }
-  if ( $request_method = POST ) {
-	return 405;
-  }
-  if ( $request_uri ~ "/wp-" ) {
-	return 405;
-  }
-  if ( $http_cookie ~ (wp-postpass|wordpress_logged_in|comment_author)_ ) {
-	return 405;
-  }
-
-  default_type text/html;
-  add_header X-Powered-By Cachify;
-  set $memcached_key $host$uri;
-  memcached_pass localhost:11211;
-}
-
-## NOCACHE LOCATION
-location @nocache {
-  try_files $uri $uri/ /index.php?$args;
-}
+REDIS_HOST=127.0.0.1
+REDIS_PORT=6379
 </textarea>
 
-<small>(<?php esc_html_e( 'You might need to adjust the location directives to your needs.', 'cachify' ); ?>)</small>
+<small>(<?php esc_html_e( 'You might need to adjust the hostname and port to your needs.', 'cachify' ); ?>)</small>
 
-<h3><?php esc_html_e( 'Notes', 'cachify' ); ?></h3>
-<ol>
-	<li>
-		<?php
-		printf(
-			/* translators: variable names*/
-			esc_html__( 'For domains with FQDN, the variable %1$s must be used instead of %2$s.', 'cachify' ),
-			'<code>${http_host}</code>',
-			'<code>${host}</code>'
-		);
-		?>
-	</li>
-	<li>
-		<?php
-		printf(
-			/* translators: code */
-			esc_html__( 'If you have errors please try to change %1$s to %2$s This forces IPv4 because some servers that allow IPv4 and IPv6 are configured to bind memcached to IPv4 only.', 'cachify' ),
-			'<code>memcached_pass localhost:11211;</code>',
-			'<code>memcached_pass 127.0.0.1:11211;</code>'
-		);
-		?>
-	</li>
-</ol>
