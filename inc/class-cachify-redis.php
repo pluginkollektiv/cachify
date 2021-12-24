98a99cca7de116512c496558a27a4940782f3a11
binaryfrost <binaryfrost@gmail.com>
2021-12-24 15:24:44 +0000

    Adding core redis functions

--- inc/class-cachify-redis.php
+++ inc/class-cachify-redis.php
@@ -14,6 +14,14 @@ defined( 'ABSPATH' ) || exit;
 final class Cachify_REDIS {
 
 	/**
+	 * Redis-Object
+	 *
+	 * @since  2.0.7
+	 * @var    object
+	 */
+	private static $_redis;
+
+	/**
 	 * Availability check
 	 *
 	 * @since   2.0.7
@@ -24,7 +32,9 @@ final class Cachify_REDIS {
 	public static function is_available() {
 		// $option = get_option( 'permalink_structure' );
 		// return ! empty( $option );
-		return true;
+		return class_exists( 'Redis' )
+			&& getenv( 'REDIS_HOST' )
+			&& getenv( 'REDIS_PORT' );
 	}
 
 	/**
@@ -51,19 +61,44 @@ final class Cachify_REDIS {
 	 * @param   bool    $sig_detail  Show details in signature.
 	 */
 	public static function store_item( $hash, $data, $lifetime, $sig_detail ) {
+		/* Do not store empty data. */
+		if ( empty( $data ) ) {
+			trigger_error( __METHOD__ . ': Empty input.', E_USER_WARNING );
+			return;
+		}
 
+		/* Server connect */
+		if ( ! self::_connect_server() ) {
+			return;
+		}
+
+		/* Add item */
+		self::$_redis->set(
+			self::_file_path(),
+			$data . self::_cache_signature( $sig_detail ),
+			$lifetime
+		);
 	}
 
 	/**
 	 * Read item from cache
 	 *
-	 * @since   2.0
-	 * @change  2.0
+	 * @since   2.0.7
+	 * @change  2.0.7
 	 *
-	 * @return  boolean  True if cache is present.
+	 * @param   string $hash  Hash of the entry.
+	 * @return  mixed         Content of the entry
 	 */
-	public static function get_item() {
-		return false;
+	public static function get_item( $hash ) {
+		/* Server connect */
+		if ( ! self::_connect_server() ) {
+			return;
+		}
+
+		/* Get item */
+		return self::$_redis->get(
+			self::_file_path()
+		);
 	}
 
 	/**
@@ -76,7 +111,15 @@ final class Cachify_REDIS {
 	 * @param   string $url   URL of the entry.
 	 */
 	public static function delete_item( $hash, $url ) {
+		/* Server connect */
+		if ( ! self::_connect_server() ) {
+			return;
+		}
 
+		/* Delete */
+		self::$_redis->del(
+			self::_file_path( $url )
+		);
 	}
 
 	/**
@@ -86,7 +129,13 @@ final class Cachify_REDIS {
 	 * @change  2.0
 	 */
 	public static function clear_cache() {
+		/* Server connect */
+		if ( ! self::_connect_server() ) {
+			return;
+		}
 
+		/* Flush */
+		@self::$_redis->flushAll();
 	}
 
 	/**
@@ -96,7 +145,7 @@ final class Cachify_REDIS {
 	 * @change  2.3
 	 */
 	public static function print_cache() {
-
+		return;
 	}
 
 	/**
@@ -108,7 +157,25 @@ final class Cachify_REDIS {
 	 * @return  integer  Directory size
 	 */
 	public static function get_stats() {
-		return 0;
+		/* Server connect */
+		if ( ! self::_connect_server() ) {
+			return null;
+		}
+
+		/* Info */
+		$data = self::$_redis->info();
+
+		/* No stats? */
+		if ( empty( $data ) ) {
+			return null;
+		}
+
+		/* Empty */
+		if ( empty( $data['used_memory'] ) ) {
+			return null;
+		}
+
+		return $data['used_memory'];
 	}
 
 	/**
@@ -130,6 +197,62 @@ final class Cachify_REDIS {
 				current_time( 'timestamp' )
 			)
 		);
+	}
+
+	/**
+	 * Path of cache file
+	 *
+	 * @since   2.0.7
+	 * @change  2.0.7
+	 *
+	 * @param   string $path  Request URI or permalink [optional].
+	 * @return  string        Path to cache file
+	 */
+	private static function _file_path( $path = null ) {
+		// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.ValidatedSanitizedInput.InputNotValidated
+		$path_parts = wp_parse_url( $path ? $path : wp_unslash( $_SERVER['REQUEST_URI'] ) );
+
+		return trailingslashit(
+			sprintf(
+				'%s%s',
+				// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.ValidatedSanitizedInput.InputNotValidated
+				wp_unslash( $_SERVER['HTTP_HOST'] ),
+				$path_parts['path']
+			)
+		);
+	}
+
+	/**
+	 * Connect to Redis server
+	 *
+	 * @return  boolean  true/false  TRUE on success
+	 */
+	private static function _connect_server() {
+		/* Not enabled? */
+		if ( ! self::is_available() ) {
+			return false;
+		}
+
+		/* Have object and it thinks it's connected to a server */
+		if (is_object( self::$_redis ) && self::$_redis->isConnected() ) {
+			return true;
+		}
+
+		/* Init */
+		self::$_redis = new \Redis();
+
+		/* Set options & connect */
+		try {
+			self::$_redis->connect( getenv( 'REDIS_HOST' ), intval( getenv( 'REDIS_PORT' ) ) );
+		} catch (Exception $e) {
+			return false;
+		}
+
+		if ( ! self::$_redis->isConnected() ) {
+			return false;
+		}
+
+		return true;
 	}
 
 }
