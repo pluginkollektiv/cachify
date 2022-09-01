2265b4daffd18ae786c9aef34a04a9fd5b367de2
dseydel <dseydel@united-internet.de>
2022-09-01 12:52:09 +0200

    Add accept header condition

--- inc/class-cachify.php
+++ inc/class-cachify.php
@@ -1258,6 +1258,13 @@ final class Cachify {
 			return true;
 		}
 
+		/* Content Negotiation */
+
+		// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.MissingUnslash, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+		if ( isset( $_SERVER['HTTP_ACCEPT'] ) && false === strpos( $_SERVER['HTTP_ACCEPT'], 'text/html' ) ) {
+			return true;
+		}
+
 		return false;
 	}
 
