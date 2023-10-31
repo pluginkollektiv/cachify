3a5a7f375d94654043356d63a666d43b476736b3
Stefan Kalscheuer <stefan@stklcode.de>
2023-10-31 17:11:54 +0100

    extend unit tests with new option

--- tests/test-cachify.php
+++ tests/test-cachify.php
@@ -114,7 +114,13 @@ class Test_Cachify extends WP_UnitTestCase {
 		$robots_txt = "User-agent: *\nDisallow: /wordpress/wp-admin/\nAllow: /wordpress/wp-admin/admin-ajax.php\n";
 
 		// DB cache enabled.
-		update_option( 'cachify' , array( 'use_apc' => Cachify::METHOD_DB ) );
+		update_option(
+			'cachify',
+			array(
+				'use_apc'           => Cachify::METHOD_DB,
+				'change_robots_txt' => 1,
+			)
+		);
 		new Cachify();
 
 		self::assertEquals(
@@ -124,8 +130,29 @@ class Test_Cachify extends WP_UnitTestCase {
 		);
 
 		// HDD cache enabled.
-		update_option( 'cachify' , array( 'use_apc' => Cachify::METHOD_HDD ) );
+		update_option(
+			'cachify',
+			array(
+				'use_apc'           => Cachify::METHOD_HDD,
+				'change_robots_txt' => 1,
+			)
+		);
 		new Cachify();
+
+		self::assertEquals(
+			$robots_txt . "\nUser-agent: *\nDisallow: */cache/cachify/\n",
+			Cachify::robots_txt( $robots_txt ),
+			'robots.tst should have been modified using HDD cache'
+		);
+
+		// Disable robots.txt modification.
+		update_option(
+			'cachify',
+			array(
+				'use_apc'           => Cachify::METHOD_HDD,
+				'change_robots_txt' => 0,
+			)
+		);
 
 		self::assertEquals(
 			$robots_txt . "\nUser-agent: *\nDisallow: */cache/cachify/\n",
